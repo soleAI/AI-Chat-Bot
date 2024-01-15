b2ca9a6ecdd826856163c19dd0e3aeada0956b4b
kanha638 <tiwarikanha26@gmail.com>
2024-01-15 16:29:37 +0000

    memory based chatbot completed

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import warnings
 
 from langchain_community.llms import OpenAI
 from langchain.prompts import PromptTemplate
+import re
 
 warnings.filterwarnings('ignore')
 
@@ -23,14 +24,42 @@ def chat_bot(input):
   return answer
 
 
+prev_chat = []
+
+
+def get_fun(inp):
+  result = ''''''
+  if len(prev_chat)<9:
+    for i in range(len(prev_chat)):
+      result+=prev_chat[i]['chat']
+    result+=inp
+  else:
+    for i in range(len(prev_chat)-9,len(prev_chat)):
+      result+=prev_chat[i]['chat']
+    result+=inp
+  return result
+    
+    
 if __name__ == '__main__':
   print("Hii ! I am you assistance how can i help you ?")
   while True:
     inp = input("You : ")
     if(inp=='exit'):
       break
-    response = chat_bot(inp)
-    print(f'''
-AI : {response}
+    inp = f'''Human : {inp}'''
+    final_inp = get_fun(inp)
+    print(final_inp)
+    response = chat_bot(final_inp)
+    response=response.strip()
+
+    res = {
+      "chat" : f'''{inp}
+{response}
+      '''
+    }
+    prev_chat.append(res)
+
+    
+    print(f'''{response.split(":")[1].strip()}
     ''',end='\n')
     
