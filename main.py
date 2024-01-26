43a125359bd478d62c41aaf19bb7d95886572e7e
kanha638 <tiwarikanha26@gmail.com>
2024-01-26 15:57:35 +0530

    added connection manager to handle multiple client

--- main.py
+++ main.py
@@ -18,18 +18,41 @@ app = FastAPI()
 
 open_ai = OpenAIService(temp=0.6)
 
+class ConnectionManager:
 
-@app.websocket("/ws")
-async def websocket_endpoint(websocket:WebSocket):
-    await websocket.accept()
+    def __init__(self) -> None:
+        self.connections : list[WebSocket] = []
 
+    async def connect(self,websocket:WebSocket) -> None:
+        await websocket.accept()
+        self.connections.append(websocket)
+
+    def disconnect(self,websocket:WebSocket) -> None:
+        self.connections.remove(websocket)
+    
+    async def send_message(self,message:str,websocket:WebSocket) -> None:
+        await websocket.send_text(message)
+
+    
+
+
+manager = ConnectionManager()
+
+
+@app.websocket("/ws/{client_id}")
+async def websocket_endpoint(websocket:WebSocket,client_id:str):
+    await manager.connect(websocket)
+    print(client_id)
     try:
         while True:
             data = await websocket.receive_text()
-            response = open_ai.get_reponse(data)
-            await websocket.send_text(f"AI : {response}")
+            response = open_ai.get_reponse(data,client_id)
+            await manager.send_message(response,websocket)
+
     except WebSocketDisconnect:
-        print("Socket closed")
+        manager.disconnect(websocket)
+        open_ai.remove(client_id)
+        print(f"Socket closed for {client_id}")
 
 
 @app.post("/bot")
--- services/openai.py
+++ services/openai.py
@@ -1,21 +1,59 @@
 
 from langchain_community.llms import OpenAI
 from langchain.prompts import PromptTemplate
+from pydantic import BaseModel
+from fastapi import WebSocket
+
+class Temp(BaseModel):
+    query : str
+    
+    
+    
+
 
 class OpenAIService:
     def __init__(self,temp:int) -> None:
         self.llm = OpenAI(temperature=temp)
+        self.prev_chat:{str:list[Temp]} = {}
+    
+    def get_fun(self,inp:str,client_id:str):
+        result:str = ''''''
+        if len(self.prev_chat[client_id])<9:
+            for i in range(len(self.prev_chat[client_id])):
+                result += self.prev_chat[client_id][i].query
+            result += inp
+        else:
+            for i in range(len(self.prev_chat[client_id])-9,len(self.prev_chat[client_id])):
+                result+=self.prev_chat[client_id][i].query
+            result+=inp
+        return result
 
-    def get_reponse(self,input:str)->str:
+    def get_reponse(self,input:str,client_id:str)->str:
         prompt_template = PromptTemplate(
             input_variables=["input"],
             template='''
                 {input}
             '''
         )
-        
-        answer:str = self.llm(prompt_template.format(input=input))
+        if client_id not in self.prev_chat.keys():
+            self.prev_chat[client_id] = []
+
+
+        inp = f'''Human : {input}'''
+        final_inp = self.get_fun(inp,client_id)
+        answer:str = self.llm(prompt_template.format(input=final_inp))
         answer:str = answer.strip()
+        
+        res:Temp = Temp(query=f'''Human : {input}
+AI : {answer}
+       ''')
+        
+        self.prev_chat[client_id].append(res)
+
         return answer
+    
+    def remove(self,client_id:str):
+        
+        self.prev_chat.pop(client_id)
 
 
