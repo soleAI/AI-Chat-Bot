87ac6104683425850b1183fcbc3f8888949fdf5c
sumit1804 <sumit2002yadav2002>
2024-03-09 23:41:42 +0530

    type define and custome formatting added

--- client/README.md
+++ client/README.md
@@ -20,11 +20,15 @@ export default {
     ecmaVersion: 'latest',
     sourceType: 'module',
     project: ['./tsconfig.json', './tsconfig.node.json'],
-    tsconfigRootDir: __dirname,
-  },
+    tsconfigRootDir: __dirname
+  }
 }
 ```
 
 - Replace `plugin:@typescript-eslint/recommended` to `plugin:@typescript-eslint/recommended-type-checked` or `plugin:@typescript-eslint/strict-type-checked`
 - Optionally add `plugin:@typescript-eslint/stylistic-type-checked`
 - Install [eslint-plugin-react](https://github.com/jsx-eslint/eslint-plugin-react) and add `plugin:react/recommended` & `plugin:react/jsx-runtime` to the `extends` list
+
+# Formatting Files in Client
+Always run this command before pushing the code.
+`npx prettier --write .`
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,32 @@
-import React from 'react';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import LoginPage from './pages/loginPage';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import SignUpPage from './pages/signUpPage';
-import ResumeUpload from './pages/ResumeUpload';
+import React from 'react'
+import {Theme, ThemeProvider, createTheme} from '@mui/material/styles'
+import LoginPage from './pages/loginPage'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import SignUpPage from './pages/signUpPage'
+import ResumeUpload from './pages/ResumeUpload'
 
-
-const App = () => {
-  const themeLight = createTheme({
+const App: React.FC = () => {
+  const themeLight: Theme = createTheme({
     palette: {
       primary: {
-        main: '#2196f3', // or any other color you prefer
+        main: '#2196f3' // or any other color you prefer
       },
       background: {
-        default: "red"
+        default: 'red'
       }
     }
-  });
+  })
   return (
     <ThemeProvider theme={themeLight}>
       <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<LoginPage/>}/>
-        <Route path='/signup' element={<SignUpPage/>}/>
-        <Route path='/resumeUpload' element={<ResumeUpload/>}/>
-      </Routes>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/resumeUpload" element={<ResumeUpload />} />
+        </Routes>
       </BrowserRouter>
     </ThemeProvider>
-  );
+  )
 }
 
-export default App;
+export default App
